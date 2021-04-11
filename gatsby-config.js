b8ff24e11437dab958dcb22a2154517c309fe94b
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2021-04-11 11:13:21 +0100

    delete gatsby plugin favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,37 +39,6 @@ module.exports = {
         display: 'swap'
       }
     },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: './src/images/favicon.png',
-
-        // WebApp Manifest Configuration
-        appName: null, // Inferred with your package.json
-        appDescription: null,
-        developerName: null,
-        developerURL: null,
-        dir: 'auto',
-        lang: 'en-US',
-        background: '#fff',
-        theme_color: '#fff',
-        display: 'standalone',
-        orientation: 'any',
-        start_url: '/',
-        version: '1.0',
-
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          firefox: true,
-          yandex: false,
-          windows: false
-        }
-      }
-    },
     `gatsby-plugin-styled-components`,
     'gatsby-plugin-typescript',
     'gatsby-plugin-sharp',
